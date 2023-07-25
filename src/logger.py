e286053e16a02ef8d7a11630b94c19dee12c6ccf
joshua maeba <joshuamaeba@gmail.com>
2023-07-25 13:34:00 +0300

    logging and exception handling

---
+++ src/logger.py
@@ -0,0 +1,17 @@
+import logging
+import os
+from datetime import datetime
+
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"  # Fixed the strftime format.
+
+logs_path = os.path.join(os.getcwd(), "logs")  # Changed variable name to "logs_path."
+os.makedirs(logs_path, exist_ok=True)
+
+LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+)
+
